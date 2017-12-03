7cd67d9a40e7d35fcfab08812a342ee787f67530
Luberto Klaassen <luberto.klaassen@virtualsciences.nl>
2017-12-03 17:55:04 +0100

    Add puppet install to custom script

--- build-vbox.bat
+++ build-vbox.bat
@@ -1,4 +1,4 @@
 @echo off
 packer build -only=virtualbox-iso -var-file=centos7-desktop.json centos.json
 vagrant box remove centos7-desktop
-vagrant box add centos7-desktop file:///D:/Virtual%20Machines/Packer/centos/box/virtualbox/centos7-desktop-0.0.99.box --box-version 0.0.99
+vagrant box add centos7-desktop file:///D:/Virtual%20Machines/Packer/centos/box/virtualbox/centos7-desktop-0.0.99.box rem --box-version 0.0.99
--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,10 @@ set -eux
 #
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
+yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet/puppet-release-el-7.noarch.rpm
+yum install -y --nogpgcheck puppet
+/opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri
+/opt/puppetlabs/puppet/bin/gem install generate-puppetfile
+sed -i '/secure_path/ s/$/:\/opt\/puppetlabs\/puppet\/bin/' /etc/sudoers
+echo 'Defaults env_keep += "SSH_AUTH_SOCK"' >> /etc/sudoers.d/ssh-auth-sock
+chmod 0440 /etc/sudoers.d/ssh-auth-sock
